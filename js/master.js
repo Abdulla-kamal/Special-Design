1a189a220c95f9ca0c65af4914ef4839397cbb75
Abdulla-Kamal <elhassiabdo@gmail.com>
2023-12-25 18:52:12 +0200

    Abdout_Us section

--- js/master.js
+++ js/master.js
@@ -2,8 +2,9 @@
 let landingPage = document.querySelector(".landing-page");
 let imgContainer = document.querySelectorAll(".image-box .images-list img");
 let startBtn = document.querySelector(".image-box .buttons .start");
-console.log(startBtn)
 let stopBtn = document.querySelector(".image-box .buttons .stop");
+let change = true;
+let intervalValue;
 
 
 //CHeck On Local Storage Color Option
@@ -22,7 +23,13 @@ if (localStorage.getItem("color") !== null) {
     });
     startBtn.style.backgroundColor = localStorage.getItem("color");
     stopBtn.style.backgroundColor = localStorage.getItem("color");
-}
+  }
+
+  // Check IF Random In Local Storage 
+  let randomImage = localStorage.getItem("random");
+  if(randomImage !== null) {
+    landingPage.style.backgroundImage = `url(../images/0${randomImage}.webp)`;
+  }
 
 if(localStorage.getItem("image") !== null) {
   document.querySelector(".landing-page").style.backgroundImage = `url(../${localStorage.getItem("image")})`;
@@ -34,6 +41,23 @@ if(localStorage.getItem("image") !== null) {
   })
 }
 
+// Check in Local Storage Buttons And Add Class Transparent 
+let backgroundChange = localStorage.getItem("change");
+if(backgroundChange !== null) {
+  if(backgroundChange === 'true') {
+    change = true;
+  }else {
+    change =  false;
+  }
+  document.querySelectorAll(".image-box .buttons button").forEach(btn=> {
+    btn.classList.remove("transparent")
+  })
+  if(backgroundChange === 'true') {
+    startBtn.classList.add("transparent");
+  }else {
+    stopBtn.classList.add("transparent");
+  }
+}
 
 //Settings Box
 // Toggle Classes
@@ -80,34 +104,68 @@ imgContainer.forEach(img => {
 
 
 
-
-// Landing Page Backgorund
-let counter = setInterval(() => {
-  let randomNum = Math.ceil(Math.random() * 5);
-  landingPage.style.backgroundImage = `url(../images/0${randomNum}.webp)`;
-}, 5000);
-
-
-// Remove And Add Class Active To Button 
+// Remove And Add Class Active From Button 
 document.querySelectorAll(".settings-box .buttons button").forEach(btn=> {
   btn.addEventListener('click', (e)=> {
-    e.target.parentElement.querySelectorAll(".active").forEach(actBtn=> {
-      actBtn.classList.remove("active");
+    e.target.parentElement.querySelectorAll(".transparent").forEach(actBtn=> {
+      actBtn.classList.remove("transparent");
     })
-    e.target.classList.add("active");
+    e.target.classList.add("transparent");
+    if(e.target.dataset.background === "yes") {
+      change = true;
+      randomBackground();
+      localStorage.setItem("change", change);
+    }else {
+      change = false;
+      clearInterval(intervalValue);
+      localStorage.setItem("change", change);
+    }
   })
 })
 
 
 
-
-
-
-//Click On Stop Changing Bottom
-if(document.querySelectorAll(".settings-box .buttons button")[0].classList[1] )
+  // Changing Random Background Function
+  function randomBackground() {
+   if(change) {
+    intervalValue = setInterval(() => {
+      let randomNum = Math.ceil(Math.random() * 5);
+      landingPage.style.backgroundImage = `url(../images/0${randomNum}.webp)`;
+      localStorage.setItem("random", randomNum);
+    }, 2000);
+   }
+  }
+  
+  
+  
+  //Click On Stop Changing Bottom
+  if(document.querySelectorAll(".settings-box .buttons button")[0].classList[1] )
 stopBtn.addEventListener("click", function() {
-  clearInterval(counter);
   document.querySelectorAll(".image-box .images-list img").forEach(img=> {
-    img.classList.remove("active")
+    img.classList.removeo("active");
   })
 })
+randomBackground();
+
+
+
+// Visibl THe Text When Scroll 
+
+//Select About Us Section
+let about = document.querySelector(".about");
+console.log(about);
+
+window.onscroll = function() {
+let aboutOffset = about.offsetTop;
+let aboutOutter = about.offsetHeight;
+let widowHeight = this.innerHeight;
+let scrolly = this.pageYOffset;
+console.log(aboutOutter)
+if(scrolly > (aboutOffset + aboutOutter - (widowHeight +50))) {
+  console.log("You Reached");
+  document.querySelector(".about .content .text p").className = "visible"
+  document.querySelector(".about  h2").className = "visible"
+}
+
+// console.log(scrolly)
+}
